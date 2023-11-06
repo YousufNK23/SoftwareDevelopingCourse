86aae9c0124cb47901f93702a7700ce4cbd8e098
Yousuf Naveed Khan <129192243+YousufNaveedKhan@users.noreply.github.com>
2023-11-06 17:09:01 +0500

    Code Updated

--- PHP Assignments/CV Uploader/index.php
+++ PHP Assignments/CV Uploader/index.php
@@ -41,7 +41,6 @@ if (!$con) {
   <title>CV Uploader | YNK Mades</title>
   <link rel="stylesheet" href="css/style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  <link rel="stylesheet" href="//cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
 </head>
 
 <body>
@@ -93,7 +92,7 @@ if (!$con) {
 
   <div class="container">
 
-    <table class="table table-hover" id="myTable">
+    <table class="table table-hover">
       <thead>
         <tr>
           <th scope="col">#</th>
@@ -138,13 +137,7 @@ if (!$con) {
     </table><br><br>
 
     <script src="js/delete.js"></script>
-    <script src="//cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script>
-      $(document).ready(function() {
-        $('#myTable').DataTable();
-      });
-    </script>
 </body>
 
 </html>
