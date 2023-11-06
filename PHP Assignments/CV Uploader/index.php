9af95b539e78af6556ef9933fcb552aa044a681e
Yousuf Naveed Khan <129192243+YousufNaveedKhan@users.noreply.github.com>
2023-11-06 17:06:13 +0500

    Code Updated

--- PHP Assignments/CV Uploader/index.php
+++ PHP Assignments/CV Uploader/index.php
@@ -41,8 +41,7 @@ if (!$con) {
   <title>CV Uploader | YNK Mades</title>
   <link rel="stylesheet" href="css/style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  <!-- Add the PDFObject library -->
-  <script src="pdfobject.min.js"></script>
+  <link rel="stylesheet" href="//cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
 </head>
 
 <body>
